9d1fb51d1b892e1d7e110d2dc0faff57139c9088
ik3nna <iamiykendukwu@gmail.com>
2023-11-14 11:53:03 +0100

    feat: Initial setup

---
+++ src/utils/fonts.ts
@@ -0,0 +1,11 @@
+export const customFonts = {
+    "poppins_black": require("../assets/fonts/Poppins-Black.ttf"),
+    "poppins_bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "poppins_extrabold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "poppins_extralight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "poppins_light": require("../assets/fonts/Poppins-Light.ttf"),
+    "poppins_medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "poppins_regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "poppins_semibold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "poppins_thin": require("../assets/fonts/Poppins-Thin.ttf"),
+}
